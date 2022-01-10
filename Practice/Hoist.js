10f37dae5c255eb667154e2002d773c049409bf4
WinterP <winterp.dev@gmail.com>
2022-01-10 15:16:21 -0600

    Initialize Repo

---
+++ Practice/Hoist.js
@@ -0,0 +1,13 @@
+// GIVEN
+// console.log(example);
+// var example = "I'm the example!";
+// AFTER HOISTING BY THE INTERPRETER
+// var example;
+// console.log(example); // logs undefined
+// example = "I'm the example!";
+
+
+console.log(hello);
+var hello = 'world';
+// returns undefined due to var declaration after the console log. 
+
